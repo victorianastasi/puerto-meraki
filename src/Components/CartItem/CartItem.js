592ec4caf90a3530024becc19ddbf9f4953b0e43
victorianastasi <victorianastasi@gmail.com>
2023-08-27 12:37:06 -0300

    fix: cart item

--- src/Components/CartItem/CartItem.js
+++ src/Components/CartItem/CartItem.js
@@ -1,30 +1,37 @@
-import React, { useContext } from 'react';
-import './CartItem.css';
-import { FaTrashAlt } from 'react-icons/fa';
-import { CartContext } from '../../context/CartContext';
+import React, { useContext } from "react";
+import "./CartItem.css";
+import { FaTrashAlt } from "react-icons/fa";
+import { CartContext } from "../../context/CartContext";
 
 const CartItem = ({ item, quantity, id }) => {
-    const { removeItem } = useContext(CartContext);
+  const { removeItem } = useContext(CartContext);
 
-    return (
-        <div className="row row-item">
-            <div className="col-12 col-sm-3">
-                <img src={item.pictureUrl} className="cart-item-image" alt={item.title}/>
-            </div>
-            <div className="col-12 col-sm-3">
-                <p className="item-cart-text">{item.title}</p>
-            </div>
-            <div className="col-12 col-sm-2">
-                <p className="item-cart-text">Cantidad: {quantity}</p>
-            </div>
-            <div className="col-12 col-sm-2">
-                <p className="item-cart-text">Precio: $ {item.price * quantity}</p>
-            </div>
-            <div className="col-12 col-sm-2">
-                <button className="btn btn-dark btn-remove" onClick={() => removeItem(id)}><FaTrashAlt /></button>
-            </div> 
+  return (
+    <div className="row-items-container">
+      <div className="row-items">
+        <div className="item-cart item-cart-image">
+          <img src={item.pictureUrl} alt={item.title} />
         </div>
-    )
-}
+        <div className="item-cart">
+          <p className="item-cart-text">{item.title}</p>
+        </div>
+        <div className="item-cart">
+          <p className="item-cart-text">Cantidad: {quantity}</p>
+        </div>
+        <div className="item-cart">
+          <p className="item-cart-text">Precio: $ {item.price * quantity}</p>
+        </div>
+        <div className="item-cart">
+          <button
+            className="btn btn-dark btn-remove"
+            onClick={() => removeItem(id)}
+          >
+            <FaTrashAlt />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CartItem;
